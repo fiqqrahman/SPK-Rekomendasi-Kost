a620e32d4bb01d5cc9f42af95089538fd67a6e87
Taufiqurahman <fiqqrahman.official@gmail.com>
2026-08-05 13:28:30 +0700

    .

--- app/Config/Security.php
+++ app/Config/Security.php
@@ -82,5 +82,6 @@ class Security extends BaseConfig
      *
      * @see https://codeigniter4.github.io/userguide/libraries/security.html#redirection-on-failure
      */
-    public bool $redirect = (ENVIRONMENT === 'production');
+    // public bool $redirect = (ENVIRONMENT === 'production');
+    public bool $redirect = false; // Nonaktifkan redirect untuk menampilkan pesan error CSRF di halaman yang sama
 }
